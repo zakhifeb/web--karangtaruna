f52f9123ee4c66f5a4734636428c844b43826298
zakhifebri <Sutriaander@gmail.com>
2026-04-25 04:34:27 +0700

    Fix voter fillable user_id

--- app/Models/Voter.php
+++ app/Models/Voter.php
@@ -6,9 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Voter extends Model
 {
-protected $fillable = [
-    'nama',
-    'nik',
-    'status_vote'
+    protected $fillable = [
+        'nama',
+        'nik',
+        'user_id',
+        'status_vote'
     ];
 }
